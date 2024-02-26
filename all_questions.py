51a1f03b04b1822d1b73fc3791d0f0c0341d154d
gaganullas <121843706+gaganullas@users.noreply.github.com>
2024-02-25 20:52:03 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -233,10 +233,10 @@ def question7():
     answers = {}
 
     # type: string
-    answers["(a)"] = ""
+    answers["(a)"] = "Cluster 4"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain"] = ""
+    answers["(a) explain"] = " The cluster with the largest clustering entropy is Cluster 4 because it has a more balanced distribution of objects across different classes compared to the other clusters."
 
     # type: string
     answers["(b)"] = ""
